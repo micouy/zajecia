f5fa60ee800369836ec6d32b4dd19f88c0c5ed74
Mikołaj Powierża <szpontaniczny@gmail.com>
2018-11-15 17:46:20 +0100

    Create hello-world.py

---
+++ lekcja-1/hello-world.py
@@ -0,0 +1,7 @@
+# linijka z hasztagiem na początku jest ignorowana
+# takie linijki nazywane są komentarzami
+
+# polecenie `print` wypisuje wartość argumentu (argumenty wypisuje się między nawiasami)
+print("hello world")
+
+# uruchomienie tego programu spowoduje wypisanie "hello world" w wierszu poleceń
